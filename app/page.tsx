3e07e8fc55536c53e4fca562eb42fae788cf3836
KYs <amitkys59@gmail.com>
2024-10-27 02:31:28 +0530

    added singleton prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
-export default function home() {
+import prisma from "@/lib/db"
+export default async function home() {
+  const data = prisma.contribution.findMany();
+  console.log(data);
   return (
     <div>
       this is home page
